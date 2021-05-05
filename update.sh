0d8e7bf0514ce2d3d8cde769298b43c7e7b90ffd
bob <bob@bobdeMacBook-Pro.local>
2021-05-06 01:29:54 +0800

    fix

--- update.sh
+++ update.sh
@@ -4,7 +4,6 @@ SIDECAR_FILE="_sidebar.md"
 
 echo -e "# Pyfdtic Documents \n" > $INDEX_FILE
 cat $SIDECAR_FILE >> $INDEX_FILE
-gsed -i 's/^* /## /g' $INDEX_FILE
-gsed -i 's/  * /- /g' $INDEX_FILE
+gsed -i 's/^* /\n## /g' $INDEX_FILE
 
 gitci
