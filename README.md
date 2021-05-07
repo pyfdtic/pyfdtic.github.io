64d52d8412a37ca1e78042af7896e80f159f977b
bob <bob@bobdeMacBook-Pro.local>
2021-05-07 19:33:27 +0800

    fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
--e # Pyfdtic Documents 
+# Pyfdtic Documents 
 
 
 ## Kubernetes
@@ -62,6 +62,6 @@
   * [变量](/documents/ansible/Ansible-变量)
   * [流程控制](/documents/ansible/Ansible-流程控制)
   * [API](/documents/ansible/Ansible-API)
--e 
+
 ## AboutMe
  Email: `MjAxNi5ib2IuYmlAZ21haWwuY29t  (base64 encode)`
--- update.sh
+++ update.sh
@@ -3,10 +3,10 @@ INDEX_FILE="README.md"
 SIDECAR_FILE="_sidebar.md"
 
 ## genarate index.html file(README.md)
-echo -e "# Pyfdtic Documents \n" > $INDEX_FILE
+echo "# Pyfdtic Documents \n" > $INDEX_FILE
 cat $SIDECAR_FILE >> $INDEX_FILE
 gsed -i 's/^* /\n## /g' $INDEX_FILE
 
-echo -e '\n## AboutMe\n Email: `MjAxNi5ib2IuYmlAZ21haWwuY29t  (base64 encode)`' >> $INDEX_FILE
+echo '\n## AboutMe\n Email: `MjAxNi5ib2IuYmlAZ21haWwuY29t  (base64 encode)`' >> $INDEX_FILE
 
 gitci
